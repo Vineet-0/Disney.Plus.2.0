56a8042b823ff77726aa4ee59048c842e462d6f8
Vineet-0 <vineet351016@gmail.com>
2023-07-15 15:11:01 +0530

    CSS

---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key='ad703c5aeb682745ff964da05005cf2b'
+
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=ad703c5aeb682745ff964da05005cf2b';
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=ad703c5aeb682745ff964da05005cf2b
+const getTrendingVideos=axios.get(movieBaseUrl+"/trending/all/day?api_key="+api_key);
+const getMovieByGenreId=(id)=> axios.get(movieByGenreBaseURL+"&with_genres="+id)
+
+export default{
+    getTrendingVideos,
+    getMovieByGenreId
+}
