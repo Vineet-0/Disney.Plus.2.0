be96d8a8a2d78d9b89b7709ce54b6142252827f8
Vineet-0 <vineet351016@gmail.com>
2023-11-01 23:59:32 +0530

    carousel title issue

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -73,9 +73,11 @@ const Carousel = ({children: slides,titles}) => {
             </div>
         </div>
 
-        <div className="bg-black bg-opacity-40 px-3 py-1 md:px-6 md:py-2 absolute top-3 right-3 md:top-6 md:right-6 text-white p-1 rounded-lg">
+        {titles[curr] !== undefined && (
+          <div className="bg-black bg-opacity-40 px-3 py-1 md:px-6 md:py-2 absolute top-3 right-3 md:top-6 md:right-6 text-white p-1 rounded-lg">
             <h2 className="text-sm md:text-5xl">{titles[curr]}</h2>
-        </div>
+          </div>
+        )}
 
     </div>
   )
