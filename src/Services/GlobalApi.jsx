5b13903fa8faf54dda417cef212202488be78e53
Vineet-0 <vineet351016@gmail.com>
2023-11-28 13:51:06 +0530

    Final Data Encapsulation

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const api_key = import.meta.env.VITE_MOVIE_API_KEY;
-console.log("api_key");
-console.log(api_key);
 
-const movieBaseUrl="https://api.themoviedb.org/3"
+const movieBaseUrl = "https://api.themoviedb.org/3"
 
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key='+api_key;
+const movieByTrendingVideos = movieBaseUrl+"/trending/all/day?api_key="+api_key
+const movieByGenreBaseURL = movieBaseUrl+'/discover/movie?api_key='+api_key+'&with_genres=';
 
-const getTrendingVideos=axios.get(movieBaseUrl+"/trending/all/day?api_key="+api_key);
-const getMovieByGenreId=(id)=> axios.get(movieByGenreBaseURL+"&with_genres="+id)
+const getTrendingVideos=axios.get(movieByTrendingVideos);
+const getMovieByGenreId=(id)=> axios.get(movieByGenreBaseURL+id)
 
 export default{
     getTrendingVideos,
