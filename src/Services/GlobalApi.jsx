11f3cffb27992017928589520746ffb35d2800e3
Vineet-0 <vineet351016@gmail.com>
2023-11-28 13:37:28 +0530

    Data Encapsulation

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+const api_key = import.meta.env.VITE_MOVIE_API_KEY;
+
 const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key='ad703c5aeb682745ff964da05005cf2b'
 
 const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key='+api_key;
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=ad703c5aeb682745ff964da05005cf2b
 const getTrendingVideos=axios.get(movieBaseUrl+"/trending/all/day?api_key="+api_key);
 const getMovieByGenreId=(id)=> axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
