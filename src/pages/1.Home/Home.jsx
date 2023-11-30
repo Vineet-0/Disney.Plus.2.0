26e05895c79c835c8ac8dc8a7ee7ef4e58705077
Vineet-0 <vineet351016@gmail.com>
2023-11-30 12:52:49 +0530

    Slider-->slider

--- src/pages/1.Home/Home.jsx
+++ src/pages/1.Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Slider from '../../components/Slider/Slider.jsx'
+import Slider from '../../components/slider/Slider.jsx'
 import OTT_House from '../../components/OTT_House/OTT_House.jsx'
 import GenreMovieList from '../../components/GenreMovieList/GenreMovieList.jsx'
 
